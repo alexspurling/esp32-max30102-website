b4bcb4d6862701a9ec9eacc0acaee896451a1486
Alex Spurling <alexspurling@gmail.com>
2020-05-12 21:06:57 +0100

    Remove old http server

--- main/heartrate.c
+++ main/heartrate.c
@@ -10,12 +10,16 @@
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
+#include "esp_event_loop.h"
 #include "nvs_flash.h"
 
+#include "lwip/api.h"
 #include "lwip/err.h"
 #include "lwip/sockets.h"
 #include "lwip/sys.h"
 #include <lwip/netdb.h>
+
+#include "driver/ledc.h"
 
 #include "driver/i2c.h"
 #include "sdkconfig.h"
@@ -58,6 +62,8 @@ float heartrate = 99.2, pctspo2 = 99.2;
 static QueueHandle_t client_queue;
 const static int client_queue_size = 100;
 
+static ledc_channel_config_t ledc_channel;
+
 static _Bool starts_with(const char *restrict string, const char *restrict prefix) {
     while (*prefix) {
         if (*prefix++ != *string++) {
@@ -75,192 +81,6 @@ static void send_file(int sock, uint8_t file_start[], uint8_t file_end[]) {
         if ((file_len - pkt_ptr) < pkt_buf_size) pkt_end = file_len - pkt_ptr;
         send(sock, file_start + pkt_ptr, pkt_end, 0);
     }
-}
-
-_Noreturn static void tcp_server_task(void *pvParameters) {
-    char rx_buffer[1500];
-    char addr_str[16];
-    //static float rxv[5], ryv[5], irxv[5], iryv[5];
-    int addr_family;
-    int ip_protocol;
-    char *temp;
-
-    // index page
-    extern uint8_t index_html_start[] asm("_binary_index_html_start");
-    extern uint8_t index_html_end[] asm("_binary_index_html_end");
-
-    // default page
-    extern const uint8_t root_html_start[] asm("_binary_root_html_start");
-    extern const uint8_t root_html_end[] asm("_binary_root_html_end");
-    const uint32_t root_html_len = root_html_end - root_html_start;
-
-    // test.js
-    extern const uint8_t test_js_start[] asm("_binary_test_js_start");
-    extern const uint8_t test_js_end[] asm("_binary_test_js_end");
-    const uint32_t test_js_len = test_js_end - test_js_start;
-
-    // test.css
-    extern const uint8_t test_css_start[] asm("_binary_test_css_start");
-    extern const uint8_t test_css_end[] asm("_binary_test_css_end");
-    const uint32_t test_css_len = test_css_end - test_css_start;
-
-    // favicon.ico
-//    extern uint8_t favicon_ico_start[] asm("_binary_favicon_ico_start");
-//    extern uint8_t favicon_ico_end[] asm("_binary_favicon_ico_end");
-//    const uint32_t favicon_ico_len = favicon_ico_end - favicon_ico_start;
-
-    // error page
-    extern const uint8_t error_html_start[] asm("_binary_error_html_start");
-    extern const uint8_t error_html_end[] asm("_binary_error_html_end");
-    const uint32_t error_html_len = error_html_end - error_html_start;
-
-    char options_header[] = "HTTP/1.1 204 No Content\n"
-                            "Access-Control-Allow-Origin: * \n"
-                            "Access-Control-Allow-Headers: X-PINGOTHER, Content-Type \n"
-                            "Access-Control-Allow-Methods: GET, OPTIONS \n"
-                            "Access-Control-Max-Age: 1728000 \n";
-
-    while (1) {
-        struct sockaddr_in destAddr;
-        destAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-        destAddr.sin_family = AF_INET;
-        destAddr.sin_port = htons(PORT);
-        addr_family = AF_INET;
-        ip_protocol = IPPROTO_IP;
-        inet_ntoa_r(destAddr.sin_addr, addr_str, sizeof(addr_str) - 1);
-
-        int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
-        bind(listen_sock, (struct sockaddr *) &destAddr, sizeof(destAddr));
-        listen(listen_sock, 1);
-
-        struct sockaddr_in sourceAddr;
-        uint addrLen = sizeof(sourceAddr);
-        while (1) {
-            int sock = accept(listen_sock, (struct sockaddr *) &sourceAddr, &addrLen);
-            if (sock < 0) {
-                ESP_LOGE("", "Unable to accept connection: errno %d", errno);
-                break;
-            }
-            //ESP_LOGI("", "Socket accepted");
-            int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-            rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
-            // Error occurred during receiving
-            if (len <= 0) {
-                ESP_LOGE("", "recv failed: errno %d", errno);
-                break;
-            } else {
-                //prints client source ip and http request packet to esp monitor
-                inet_ntoa_r(((struct sockaddr_in *) &sourceAddr)->sin_addr.s_addr,
-                            addr_str, sizeof(addr_str) - 1);
-                ESP_LOGI("---  ", "Received %d bytes from %s:", len, addr_str);
-//                ESP_LOGI("", "HTTP REQUEST Packet\n%s", rx_buffer);
-                //parse request
-//                char http_type[8];
-//                char temp_str[16];
-//                sscanf(rx_buffer, "%s/", http_type);
-//                //ESP_LOGI("", "request type %s  ", http_type);
-//                temp = strstr(rx_buffer, "?");
-//                if (temp) {
-//                    memcpy(temp_str, rx_buffer + strlen(http_type) + 2,
-//                           strlen(rx_buffer) - strlen(temp));
-//                    temp_str[strlen(temp_str)] = '\0';
-//                } else {
-//                    sscanf(rx_buffer + strlen(http_type) + 2, "%s", temp_str);
-//                }
-
-                if (starts_with(rx_buffer, "OPTIONS")) {
-                    send(sock, options_header, strlen(options_header), 0);
-                } else if (starts_with(rx_buffer, "GET /index.html") || starts_with(rx_buffer, "GET / ")) {
-                    send_file(sock, index_html_start, index_html_end);
-//                } else if (starts_with(rx_buffer, "GET /favicon.ico")) {
-//                    send_file(sock, favicon_ico_start, favicon_ico_end);
-                } else if (starts_with(rx_buffer, "GET /getData")) {
-                    //parse datagram, rcv data from webpage and return collected data
-                    temp = strstr(rx_buffer, "irpower=");
-                    if (temp)sscanf(temp, "irpower=%d", &irpower);
-                    temp = strstr(rx_buffer, "xrpower=");
-                    if (temp)sscanf(temp, "xrpower=%d", &rpower);
-                    temp = strstr(rx_buffer, "raworbp=");
-                    if (temp)sscanf(temp, "raworbp=%d", &raworbp);
-                    temp = strstr(rx_buffer, "startstop=");
-                    if (temp)sscanf(temp, "startstop=%d", &startstop);
-                    //printf ("ir=%d r=%d ss=%d braw=%d\n", irpower,rpower,startstop,raworbp);
-
-                    //int adc_read_ptr_shadow = adc_read_ptr;
-                    //adc_read_ptr = 0;
-
-                    char outstr[2000];
-                    /*
-                              snprintf( outstr, sizeof outstr, "%d,",adc_read_ptr_shadow / 3);
-                              snprintf(tmp, sizeof tmp, "%ld,", adc_read[0]); strcat (outstr, tmp);
-                              snprintf(tmp, sizeof tmp, "%ld,", avgIR); strcat (outstr, tmp);
-                              snprintf(tmp, sizeof tmp, "%ld,", avgR); strcat (outstr, tmp);
-                              avgR = 0; avgIR = 0;
-                              //filter is 2nd order butterworth 0.5/25 Hz
-                              for( x = 0; x < adc_read_ptr_shadow; x++){
-                                 if(x%3 == 1){
-                                     avgR = avgR + adc_read[x];
-                                     rxv[0] = rxv[1]; rxv[1] = rxv[2]; rxv[2] = rxv[3]; rxv[3] = rxv[4];
-                                     rxv[4] = ((float) adc_read[x]) / 3.48311;
-                                     ryv[0] = ryv[1]; ryv[1] = ryv[2]; ryv[2] = ryv[3]; ryv[3] = ryv[4];
-                                     ryv[4] = (rxv[0] + rxv[4]) - 2 * rxv[2]
-                                            + ( -0.1718123813 * ryv[0]) + (  0.3686645260 * ryv[1])
-                                            + ( -1.1718123813 * ryv[2]) + (  1.9738037992 * ryv[3]);
-                                     if(raworbp==1)snprintf(tmp, sizeof tmp, "%5.1f,", rxv[4]);
-                                        else snprintf(tmp, sizeof tmp, "%5.1f,", -1.0 * ryv[4]);
-                                     strcat (outstr, tmp);
-
-                       }
-                                 if(x%3 == 2){
-                                     avgIR = avgIR + adc_read[x];
-                                     irxv[0] = irxv[1]; irxv[1] = irxv[2]; irxv[2] = irxv[3]; irxv[3] = irxv[4];
-                                     irxv[4] = ((float) adc_read[x]) / 3.48311;
-                                     iryv[0] = iryv[1]; iryv[1] = iryv[2]; iryv[2] = iryv[3]; iryv[3] = iryv[4];
-                                     iryv[4] = (irxv[0] + irxv[4]) - 2 * irxv[2]
-                                             + ( -0.1718123813 * iryv[0]) + (  0.3686645260 * iryv[1])
-                                             + ( -1.1718123813 * iryv[2]) + (  1.9738037992 * iryv[3]);
-                                     if(raworbp==1)snprintf(tmp, sizeof tmp, "%5.4f,", irxv[4]);
-                                        else   snprintf(tmp, sizeof tmp, "%5.4f,", -1.0 * iryv[4]);
-                                     strcat (outstr, tmp); }
-                              }
-                              strcat (outstr, "0\0");
-                              //ESP_LOGI("","sizeof=%d  %s",strlen(outstr), outstr);
-                              //printf("%s\n",outstr);
-                              send(sock, outstr, sizeof outstr, 0);
-                              */
-
-                    snprintf(outstr, sizeof outstr, "%2d,%2d,%4.1f,%4.1f,", countedsamples, (int) (100 * meastime),
-                             heartrate, pctspo2);
-                    strcat(outstr, outStr);  //header and data for outstr
-                    int contentLength = strlen(outstr);
-                    ESP_LOGI("", "Printing %d bytes", contentLength);
-
-                    char header[100];
-                    sprintf(header, "HTTP/1.1 200 OK\n"
-                                    "Access-Control-Allow-Origin: *\n"
-                                    "Content-Type: text/plain\n"
-                                    "Content-Length: %d\n\n", contentLength);
-
-                    send(sock, header, strlen(header), 0);  //send header for tcp responce
-                    send(sock, outstr, contentLength, 0);  //send outstr for tcp responce
-
-                    memset(outStr, 0, sizeof outStr);
-                    countedsamples = 0;
-
-
-                    //resource not found
-                } else {
-                    char temp404[] = "<html><h1>Not Found 404</html>";
-                    send(sock, temp404, sizeof(temp404), 0);
-                    //ESP_LOGI("", "404");
-                }
-                vTaskDelay(10 / portTICK_RATE_MS); //waits for buffer to purge
-                shutdown(sock, 0);
-                close(sock);
-            }
-        }
-    }
-    vTaskDelete(NULL);
 }
 
 void max30102_init() {
@@ -389,28 +209,66 @@ _Noreturn void max30102_task() {
         }
     }
 }
-
-_Noreturn static void count_task(void* pvParameters) {
-    const static char* TAG = "count_task";
-    char out[20];
-    int len;
-    int clients;
-    const static char* word = "%i";
-    int n = 0;
-    const int DELAY = 1; // / portTICK_PERIOD_MS; // 1 second
-
-    ESP_LOGI(TAG,"starting task");
-    for(;;) {
-        len = sprintf(out,word,n);
-//        clients = ws_server_send_text_all(out,len);
-        if(clients > 0) {
-            //ESP_LOGI(TAG,"sent: \"%s\" to %i clients",out,clients);
-        }
-        n++;
-        vTaskDelay(DELAY);
-    }
+// sets up the led for pwm
+static void led_duty(uint16_t duty) {
+    static uint16_t val;
+    static uint16_t max = (1L<<10)-1;
+    if(duty > 100) return;
+    val = (duty * max) / 100;
+    ledc_set_duty(ledc_channel.speed_mode,ledc_channel.channel,val);
+    ledc_update_duty(ledc_channel.speed_mode,ledc_channel.channel);
 }
 
+// handles websocket events
+void websocket_callback(uint8_t num,WEBSOCKET_TYPE_t type,char* msg,uint64_t len) {
+    const static char* TAG = "websocket_callback";
+    int value;
+
+    switch(type) {
+        case WEBSOCKET_CONNECT:
+            ESP_LOGI(TAG,"client %i connected!",num);
+            break;
+        case WEBSOCKET_DISCONNECT_EXTERNAL:
+            ESP_LOGI(TAG,"client %i sent a disconnect message",num);
+            led_duty(0);
+            break;
+        case WEBSOCKET_DISCONNECT_INTERNAL:
+            ESP_LOGI(TAG,"client %i was disconnected",num);
+            break;
+        case WEBSOCKET_DISCONNECT_ERROR:
+            ESP_LOGI(TAG,"client %i was disconnected due to an error",num);
+            led_duty(0);
+            break;
+        case WEBSOCKET_TEXT:
+            if(len) { // if the message length was greater than zero
+                switch(msg[0]) {
+                    case 'L':
+                        if(sscanf(msg,"L%i",&value)) {
+                            ESP_LOGI(TAG,"LED value: %i",value);
+                            led_duty(value);
+                            ws_server_send_text_all_from_callback(msg,len); // broadcast it!
+                        }
+                        break;
+                    case 'M':
+                        ESP_LOGI(TAG, "got message length %i: %s", (int)len-1, &(msg[1]));
+                        break;
+                    default:
+                        ESP_LOGI(TAG, "got an unknown message with length %i", (int)len);
+                        break;
+                }
+            }
+            break;
+        case WEBSOCKET_BIN:
+            ESP_LOGI(TAG,"client %i sent binary message of size %i:\n%s",num,(uint32_t)len,msg);
+            break;
+        case WEBSOCKET_PING:
+            ESP_LOGI(TAG,"client %i pinged us with message of size %i:\n%s",num,(uint32_t)len,msg);
+            break;
+        case WEBSOCKET_PONG:
+            ESP_LOGI(TAG,"client %i responded to the ping",num);
+            break;
+    }
+}
 
 
 // serves any clients
@@ -486,7 +344,7 @@ static void http_serve(struct netconn *conn) {
             }
 
             // default page
-            else if (strstr(buf,"GET / ") && !strstr(buf,"Upgrade: websocket")) {
+            else if ((strstr(buf,"GET / ") || strstr(buf,"GET /index.html ")) && !strstr(buf,"Upgrade: websocket")) {
                 ESP_LOGI(TAG,"Sending /");
                 netconn_write(conn, HTML_HEADER, sizeof(HTML_HEADER) - 1, NETCONN_NOCOPY);
                 netconn_write(conn, index_html_start, index_html_len, NETCONN_NOCOPY);
@@ -495,10 +353,20 @@ static void http_serve(struct netconn *conn) {
                 netbuf_delete(inbuf);
             }
 
+            // default page
+            else if (strstr(buf,"GET /root.html ")) {
+                ESP_LOGI(TAG,"Sending /root.html");
+                netconn_write(conn, HTML_HEADER, sizeof(HTML_HEADER) - 1, NETCONN_NOCOPY);
+                netconn_write(conn, root_html_start, root_html_len, NETCONN_NOCOPY);
+                netconn_close(conn);
+                netconn_delete(conn);
+                netbuf_delete(inbuf);
+            }
+
             // default page websocket
             else if(strstr(buf,"GET / ") && strstr(buf,"Upgrade: websocket")) {
                 ESP_LOGI(TAG,"Requesting websocket on /");
-//                ws_server_add_client(conn,buf,buflen,"/",websocket_callback);
+                ws_server_add_client(conn, buf, buflen, "/", websocket_callback);
                 netbuf_delete(inbuf);
             }
 
@@ -668,13 +536,33 @@ static void server_handle_task(void* pvParameters) {
     struct netconn* conn;
     ESP_LOGI(TAG,"task starting");
     for(;;) {
-        xQueueReceive(client_queue,&conn,portMAX_DELAY);
+        xQueueReceive(client_queue,&conn, portMAX_DELAY);
         if(!conn) continue;
         http_serve(conn);
     }
     vTaskDelete(NULL);
 }
 
+_Noreturn static void count_task(void* pvParameters) {
+    const static char* TAG = "count_task";
+    char out[20];
+    int len;
+    int clients;
+    const static char* word = "%i";
+    int n = 0;
+    const int DELAY = 1; // / portTICK_PERIOD_MS; // 1 second
+
+    ESP_LOGI(TAG,"starting task");
+    for(;;) {
+        len = sprintf(out,word,n);
+//        clients = ws_server_send_text_all(out,len);
+        if(clients > 0) {
+            //ESP_LOGI(TAG,"sent: \"%s\" to %i clients",out,clients);
+        }
+        n++;
+        vTaskDelay(DELAY);
+    }
+}
 void app_main() {
     //configure esp32 memory, wifi and i2c 
     ESP_ERROR_CHECK(nvs_flash_init());
@@ -690,9 +578,9 @@ void app_main() {
     //configure max30102 with i2c instructions
     max30102_init();
 
+    ws_server_start();
     //start tcp server and data collection tasks
     xTaskCreate(&server_task,"server_task",8192,NULL,9,NULL);
-//    xTaskCreate(tcp_server_task, "tcp_server", 8192, NULL, 4, NULL);
     xTaskCreate(&server_handle_task,"server_handle_task",4000,NULL,6,NULL);
     xTaskCreate(max30102_task, "max30102_task", 4096, NULL, 5, NULL);
     xTaskCreate(&count_task,"count_task",6000,NULL,2,NULL);
